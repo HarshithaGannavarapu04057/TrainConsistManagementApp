7d611ca05b765e5498315e6ea5a7ccd9cbf40f67
HarshithaGannavarapu04057 <harshithagannavarapu71@gmail.com>
2026-04-10 09:45:39 +0530

    UC9:Group Bogies by Type

---
+++ src/UC9.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class UC9{
+
+    // Reusing Bogie model from UC7 / UC8
+    static class Bogie {
+        String name;
+        int capacity;
+
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+
+        @Override
+        public String toString() {
+            return name + " -> " + capacity;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("UC9 - Group Bogies by Type");
+        System.out.println("==================================\n");
+
+        // Create List of bogies
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Sleeper", 70));
+        bogies.add(new Bogie("AC Chair", 68));
+
+        // Display input bogies
+        System.out.println("All Bogies:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+
+        // ---- GROUP USING Collectors.groupingBy ----
+        Map<String, List<Bogie>> groupedBogies =
+                bogies.stream()
+                        .collect(Collectors.groupingBy(b -> b.name));
+
+        // Display grouped structure
+        System.out.println("\nGrouped Bogies:");
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+
+            System.out.println("\nType: " + entry.getKey());
+
+            for (Bogie b : entry.getValue()) {
+                System.out.println("  " + b);
+            }
+        }
+    }
+}
